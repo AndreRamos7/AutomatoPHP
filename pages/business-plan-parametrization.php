fd904e3ada705c59bc0eb0280b1c08a9b255ffbb
andre <andre-rammos@hotmail.com>
2017-07-10 18:08:05 -0300

    bootstrap addicionado na páginas

--- pages/business-plan-parametrization.php
+++ pages/business-plan-parametrization.php
@@ -8,13 +8,37 @@
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+         <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="../webroot/bootstrap.min.css">
+        <!-- jQuery library -->
+        <script src="../webroot/jquery.min.js"></script>
+        <!-- Latest compiled JavaScript -->
+        <script src="../webroot/bootstrap.min.js"></script>
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PPGCC - Business-plan-parametrization</title>
+        <style>            
+            #btnlogar{                
+                float: right;
+            }
+            #cabecalho{                
+                background-color: #4CAF50;
+            }
+        </style>
     </head>
     <body>
-        <?php
         
-            echo "<h3>business-plan-parametrization</h3>";
-        ?>
+        
+         <div class="container jumbotron" id="cabecalho">
+            <?php echo "<h1>Business-plan-parametrization</h1>"; ?>         
+        </div>
+        <div class="container">
+            <h4>Business-plan-parametrization </h4>
+            <ul class="list-group">
+              <li class="list-group-item">Business nas empresas <span class="badge">12</span></li>
+              <li class="list-group-item">Business nas Universidades <span class="badge">5</span></li>
+              <li class="list-group-item">Business no município <span class="badge">3</span></li>
+            </ul>
+        </div>
     </body>
 </html>
